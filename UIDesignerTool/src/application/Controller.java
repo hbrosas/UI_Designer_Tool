73dad85d1d65993582c63d2657a1266dff363bd2
brosashazel <hazel_brosas@dlsu.edu.ph>
2017-11-05 02:24:19 +0800

    added file reader and writer

--- UIDesignerTool/src/application/Controller.java
+++ UIDesignerTool/src/application/Controller.java
@@ -1,8 +1,22 @@
 package application;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.Optional;
 import java.util.ResourceBundle;
+
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
 
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
@@ -10,16 +24,21 @@ import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
+import javafx.scene.control.TextInputDialog;
 import javafx.scene.control.TitledPane;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.layout.Pane;
+import javafx.stage.FileChooser;
 
 public class Controller implements Initializable {
+	private TextInputDialog dialog;
 	private ArrayList<Button> buttons;
 	private ArrayList<Label> labels;
 	private ArrayList<TextField> textFields;
@@ -57,6 +76,7 @@ public class Controller implements Initializable {
 	   
 	   rcLbl.setVisible(false);
 	   rcTextArea.setVisible(false);
+	   inputDialog();
 	   
 	   Layout.expandedProperty().addListener(new ChangeListener<Boolean>() {
 			@Override
@@ -68,7 +88,160 @@ public class Controller implements Initializable {
 			}
        });
    }
+   
+   public void inputDialog() {
+	   dialog = new TextInputDialog();
+	   dialog.setTitle("Save File");
+	   dialog.setHeaderText("Enter filename: (Should contain .json ex. hello.json)");
+   }
+   
+   public void saveFile(ActionEvent event) {
+       System.out.println("Save");
+       
+       Optional<String> result = dialog.showAndWait();
+	   String filename = "none.";
 
+	   if (result.isPresent()) {
+	       filename = result.get();
+	       if(filename.contains(".json")) {
+	    	   JSONRenderer jsr = new JSONRenderer(buttons, labels, textFields);
+	    	   String text = jsr.render();
+			   try (FileWriter file = new FileWriter(filename)) {
+					file.write(text);
+					file.close();
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+	       } else {
+	    	   Alert alert = new Alert(AlertType.ERROR);
+	    	   alert.setTitle("Error");
+	    	   alert.setContentText("File not saved. Must be .json");
+	    	   alert.showAndWait();	    	   
+	       }
+	   }
+   }
+   
+   public void loadFile(ActionEvent event) {
+       System.out.println("Load");
+       FileChooser fileChooser = new FileChooser();
+       File selectedFile = fileChooser.showOpenDialog(null);
+
+       if (selectedFile != null) {
+    	   if(selectedFile.getName().contains(".json")) {
+    		   try (FileReader reader = new FileReader(selectedFile)) {
+    			   JSONParser parser = new JSONParser();
+    			   
+    			   try {
+    		            Object obj = parser.parse(reader);
+
+    		            JSONObject jsonObject = (JSONObject) obj;
+    		            System.out.println(jsonObject.toJSONString());
+    		            
+    		            // BUTTONS
+    		            JSONArray buttons = (JSONArray) jsonObject.get("buttons");
+    		            System.out.println(buttons);
+    		            Iterator<JSONObject> iterator = buttons.iterator();
+    		            while (iterator.hasNext()) {
+    		                JSONObject jobj = iterator.next();
+    		                String text = (String) jobj.get("text");
+    		                double width = (double) jobj.get("width");
+    		                double height = (double) jobj.get("height");
+    		                double xPos = (double) jobj.get("xPos");
+    		                double yPos = (double) jobj.get("yPos");
+    		                addButtonLoad(text, width, height, xPos, yPos);
+    		            }
+    		            
+    		            // LABELS
+    		            JSONArray labels = (JSONArray) jsonObject.get("label");
+    		            System.out.println(labels);
+    		            iterator = labels.iterator();
+    		            while (iterator.hasNext()) {
+    		                JSONObject jobj = iterator.next();
+    		                String text = (String) jobj.get("text");
+    		                double width = (double) jobj.get("width");
+    		                double height = (double) jobj.get("height");
+    		                double xPos = (double) jobj.get("xPos");
+    		                double yPos = (double) jobj.get("yPos");
+    		                addLabelLoad(text, width, height, xPos, yPos);
+    		            }
+    		            
+    		            // TEXTFIELDS
+    		            JSONArray tfs = (JSONArray) jsonObject.get("textfield");
+    		            System.out.println(tfs);
+    		            iterator = tfs.iterator();
+    		            while (iterator.hasNext()) {
+    		                JSONObject jobj = iterator.next();
+    		                String text = (String) jobj.get("text");
+    		                double width = (double) jobj.get("width");
+    		                double height = (double) jobj.get("height");
+    		                double xPos = (double) jobj.get("xPos");
+    		                double yPos = (double) jobj.get("yPos");
+    		                addTextFieldLoad(text, width, height, xPos, yPos);
+    		            }
+    		            
+    		        } catch (ParseException e) {
+    		            e.printStackTrace();
+    		        }
+    			   
+    		   } catch (FileNotFoundException e) {
+    			   e.printStackTrace();
+    		   } catch (IOException e) {
+    			   e.printStackTrace();
+    		   }
+    	   } else {
+    		   Alert alert = new Alert(AlertType.ERROR);
+	    	   alert.setTitle("Error");
+	    	   alert.setContentText("File invalid.");
+	    	   alert.showAndWait();	 
+    	   }
+       }
+       else {
+           System.out.println("File selection cancelled.");
+       }
+   }
+
+   public void addButtonLoad(String text, double width, double height, double xPos, double yPos) {
+       System.out.println("Add Button Load");
+       System.out.println("xpos: " + xPos);
+       Button button = new Button(text);
+       button.setPrefWidth(width);
+       button.setPrefHeight(height);
+       button.setTranslateX(xPos);
+       button.setTranslateY(yPos);
+       button.setOnMousePressed(buttonOnMousePressedEventHandler);
+       button.setOnMouseDragged(buttonOnMouseDraggedEventHandler);
+       buttons.add(button);
+       workspace.getChildren().add(button);
+   }
+   
+   public void addLabelLoad(String text, double width, double height, double xPos, double yPos) {
+       System.out.println("Add Label Load");
+       Label label = new Label(text);
+       label.setPrefWidth(width);
+       label.setPrefHeight(height);
+       label.setTranslateX(xPos);
+       label.setTranslateY(yPos);
+       label.setOnMousePressed(labelOnMousePressedEventHandler);
+       label.setOnMouseDragged(labelOnMouseDraggedEventHandler);
+       labels.add(label);
+       workspace.getChildren().add(label);
+   }
+   
+   public void addTextFieldLoad(String text, double width, double height, double xPos, double yPos) {
+       System.out.println("Add TextFieeeld!");
+       
+       TextField tf = new TextField();
+       tf.setPromptText(text);
+       tf.setPrefWidth(width);
+       tf.setPrefHeight(height);
+       tf.setTranslateX(xPos);
+       tf.setTranslateY(yPos);
+       tf.setOnMousePressed(textFieldOnMousePressedEventHandler);
+       tf.setOnMouseDragged(textFieldOnMouseDraggedEventHandler);
+       textFields.add(tf);
+       workspace.getChildren().add(tf);
+   }
+   
    public void addButton(ActionEvent event) {
        System.out.println("Add Button");
        
--- UIDesignerTool/src/application/JSONRenderer.java
+++ UIDesignerTool/src/application/JSONRenderer.java
@@ -51,12 +51,10 @@ public class JSONRenderer {
 		
 		for(int i = 0; i < buttons.size(); i++) {
 			Button btn = buttons.get(i);
-			Bounds bounds = btn.getBoundsInLocal();
-	        Bounds screenBounds = btn.localToScreen(bounds);
-	        int x = (int) screenBounds.getMinX();
-	        int y = (int) screenBounds.getMinY();
-	        int width = (int) screenBounds.getWidth();
-	        int height = (int) screenBounds.getHeight();
+	        double x = btn.getTranslateX();
+	        double y = btn.getTranslateY();
+	        double width = btn.getWidth();
+	        double height = btn.getHeight();
 			
 			code += "\t{\n";
 			code += "\t\t \"text\" : \""+ btn.getText() +"\",\n";
@@ -77,12 +75,10 @@ public class JSONRenderer {
 		
 		for(int i = 0; i < labels.size(); i++) {
 			Label lbl = labels.get(i);
-			Bounds bounds = lbl.getBoundsInLocal();
-	        Bounds screenBounds = lbl.localToScreen(bounds);
-	        int x = (int) screenBounds.getMinX();
-	        int y = (int) screenBounds.getMinY();
-	        int width = (int) screenBounds.getWidth();
-	        int height = (int) screenBounds.getHeight();
+	        double x = lbl.getTranslateX();
+	        double y = lbl.getTranslateY();
+	        double width = lbl.getWidth();
+	        double height = lbl.getHeight();
 			
 	        code += "\t{\n";
 			code += "\t\t \"text\" : \""+ lbl.getText() +"\",\n";
@@ -103,12 +99,10 @@ public class JSONRenderer {
 		
 		for(int i = 0; i < textFields.size(); i++) {
 			TextField tf = textFields.get(i);
-			Bounds bounds = tf.getBoundsInLocal();
-	        Bounds screenBounds = tf.localToScreen(bounds);
-	        int x = (int) screenBounds.getMinX();
-	        int y = (int) screenBounds.getMinY();
-	        int width = (int) screenBounds.getWidth();
-	        int height = (int) screenBounds.getHeight();
+			double x = tf.getTranslateX();
+	        double y = tf.getTranslateY();
+	        double width = tf.getWidth();
+	        double height = tf.getHeight();
 			
 	        code += "\t{\n";
 			code += "\t\t \"text\" : \""+ tf.getPromptText() +"\",\n";
